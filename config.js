ddd3978180f8cb1d2fbf3c7a6027ef0382fe32c1
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-09 07:48:44 +0100

    added database settings to config

--- config.js
+++ config.js
@@ -1,22 +1,35 @@
 'use strict';
+var path = require('path');
 
 module.exports = {
   defaults: {
-      development: {
-        host: "http://jaeh:5000"
-      , PORT: 5000
+    development: {
+      host: "http://jaeh:5000"
+    , PORT: 5000
+    , db: {
+        driver: 'tingodb'
+      , database: path.join(__dirname, 'db')
       }
-    , production: {
-        host: "https://jaeh.at"
-      , PORT: 5000
+    }
+  , production: {
+      host: "https://jaeh.at"
+    , PORT: 5000
+    , db: {
+        driver: 'tingodb'
+      , database: path.join(__dirname, 'db')
       }
-    , staging: {
-        host: "https://staging.jaeh.at"
-      , PORT: 5000
+    }
+  , staging: {
+      host: "https://staging.jaeh.at"
+    , PORT: 5000
+    , db: {
+        driver: 'tingodb'
+      , database: path.join(__dirname, 'db')
+      }
     }
   }
 , heroku: {
-      remote: "jascha"
-    , staging: "jascha-staging"
+    remote: "jascha"
+  , staging: "jascha-staging"
   }
 }
